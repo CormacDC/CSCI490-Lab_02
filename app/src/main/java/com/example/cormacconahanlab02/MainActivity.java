d8bb2217ad88f9cb1740d0cd78bd221ea30e5ccc
CormacDC <60583567+CormacDC@users.noreply.github.com>
2021-02-23 18:33:38 -0500

    finished part 2

--- app/src/main/java/com/example/cormacconahanlab02/MainActivity.java
+++ app/src/main/java/com/example/cormacconahanlab02/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.cormacconahanlab02;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,29 +15,45 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final int REQUEST_CODE = 1;
+
     private Button button;
     private Intent i;
-    private EditText editText;
+    private ConstraintLayout layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        editText = findViewById(R.id.editText);
 
         button = findViewById(R.id.button);
-
         i = new Intent(this, MainActivity2.class);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s = editText.getText().toString();
-                i.putExtra("string", s);
-
-                startActivity(i);
+                startActivityForResult(i, REQUEST_CODE);
             }
         });
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        super.onActivityResult(requestCode,resultCode,data);
+
+        if (requestCode == REQUEST_CODE && resultCode == Activity.RESULT_OK){
+            Bundle extras = data.getExtras();
+            if (extras != null){
+                int imageID = extras.getInt("imageID");
+                layout = findViewById(R.id.layout);
+                layout.setBackground(getDrawable(imageID));
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/com/example/cormacconahanlab02/MainActivity2.java
+++ app/src/main/java/com/example/cormacconahanlab02/MainActivity2.java
@@ -3,23 +3,52 @@ package com.example.cormacconahanlab02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity2 extends AppCompatActivity {
 
-    TextView textView;
+    ImageView moon;
+    ImageView waterfall;
+
+    int imageID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        Intent i = getIntent();
-        String s = i.getExtras().getString("string");
+        moon = findViewById(R.id.imageView2);
+        moon.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
 
-        textView = findViewById(R.id.textView);
+                imageID = R.drawable.supermoon;
 
-        textView.setText(s);
+                finish();
+            }
+        });
+
+        waterfall = findViewById(R.id.imageView);
+        waterfall.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+
+                imageID = R.drawable.waterfall;
+
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void finish(){
+        Intent intent = new Intent();
+        intent.putExtra("imageID", imageID);
+        setResult(RESULT_OK, intent);
+        super.finish();
     }
 }
